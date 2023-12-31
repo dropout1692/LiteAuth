b5bbc23231cb3613712baddcc034f32e6db38051
Michal Marusinec <michal.marusinec@scr.sk>
2023-12-31 19:22:47 +0100

    tests init

--- backend/src/test/java/wtf/dpt/liteauth/backend/UserTests.java
+++ backend/src/test/java/wtf/dpt/liteauth/backend/UserTests.java
@@ -1,4 +1,15 @@
 package wtf.dpt.liteauth.backend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class UserTests {
+
+    @Test
+    public void dummyTest(){
+        assertEquals(1, 1);
+    }
 }
